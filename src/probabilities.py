3e335696503a5363656cf5a0b33f97b8d47f46a6
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-04-23 23:07:40 +0100

    file creation

---
+++ src/probabilities.py
@@ -0,0 +1,10 @@
+# Universidad de La Laguna
+# Escuela Superior de Ingeniería y Tecnología
+# Grado en Ingeniería Informática
+# Inteligencia Artificial Avanzada 2023-2024
+#
+# Author: Álvaro Fontenla León
+# Date: Apr 23 2024
+# Program: probabilities.py
+# This program calculates the probabilities of each word in the corpus of the training set.
+
